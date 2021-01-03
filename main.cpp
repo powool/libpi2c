b45951055d246bed638d1a449ee25eb705a86e02
Paul Anderson <pha@umich.edu>
2021-01-03 18:10:38 -0500

    cleanly shut down threads on sigint

--- main.cpp
+++ main.cpp
@@ -10,6 +10,30 @@
 #include "vcnl4040.hpp"
 #include "aht20.hpp"
 
+class checkException : std::exception {
+private:
+        std::string     msg;
+public:
+        checkException(const std::string &e);
+        const char* what();
+};
+checkException::checkException(const std::string &e) {
+        msg = e;
+}
+const char* checkException::what() {
+        return msg.c_str();
+}
+
+bool dieFlag = false;
+void die(void) {
+	dieFlag = true;
+}
+
+void checkDeath(void) {
+	if(dieFlag) throw checkException("program shutdown");
+}
+
+
 void handler(int sig) {
   void *array[10];
   size_t size;
@@ -23,19 +47,27 @@ void handler(int sig) {
   exit(1);
 }
 
+void interrupt(int sig) {
+	die();
+}
+
 void watch_humidity(std::shared_ptr<i2cBus> i2c)
 {
 	auto htSensor = std::make_shared<aht20>(i2c);
 	while(true) {
 		try {
+			checkDeath();
 			htSensor->readSensor();
 			std::cout << "temperature=" << htSensor->getTemperature() << std::endl;
 			std::cout << "humidity=" << htSensor->getHumidity() << std::endl;
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 		} catch (i2cException e) {
 			std::cerr << "device disconnect: " << e.what() << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(10));
+		} catch (checkException e) {
+			std::cerr << "device shutdown: " << e.what() << std::endl;
+			return;
 		}
-		std::this_thread::sleep_for(std::chrono::seconds(10));
 	}
 }
 
@@ -50,6 +82,7 @@ void watch_proximity(std::shared_ptr<i2cBus> i2c)
 			vcnl->setProxLowThreshold(5);
 			vcnl->enableProxLogicMode();
 			while(true) {
+				checkDeath();
 				if (vcnl->isClose()) {
 					std::cout << "Oh so close!" << std::endl;
 				}
@@ -63,8 +96,11 @@ void watch_proximity(std::shared_ptr<i2cBus> i2c)
 			}
 		} catch (i2cException e) {
 			std::cerr << "device disconnect: " << e.what() << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(10));
+		} catch (checkException e) {
+			std::cerr << "device shutdown: " << e.what() << std::endl;
+			return;
 		}
-		std::this_thread::sleep_for(std::chrono::seconds(10));
 	}
 }
 
@@ -72,17 +108,26 @@ int main(int argc, const char **argv)
 {
 	i2cBus::list_devices();
 //	signal(SIGSEGV, handler);   // install our handler
+	signal(SIGINT, interrupt);
+
+	auto bcm = std::make_shared<bcm2835>();
+	auto i2c = std::make_shared<i2cBus>(bcm);
+
+	auto humidity_thread = std::make_shared<std::thread>(watch_humidity, i2c);
+	auto proximity_thread = std::make_shared<std::thread>(watch_proximity, i2c);
 
 	try {
-		auto bcm = std::make_shared<bcm2835>();
-		auto i2c = std::make_shared<i2cBus>(bcm);
-
 		i2c->list_connected_devices();
 
-		auto humidity_thread = std::make_shared<std::thread>(watch_humidity, i2c);
-		auto proximity_thread = std::make_shared<std::thread>(watch_proximity, i2c);
-		std::this_thread::sleep_for(std::chrono::seconds(60));
+		while(true) {
+			checkDeath();
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
 	} catch (i2cException e) {
 		std::cerr << "got exception: " << e.what() << std::endl;
+	} catch (checkException e) {
+		std::cerr << "final shutdown: " << e.what() << std::endl;
 	}
+	humidity_thread->join();
+	proximity_thread->join();
 }
