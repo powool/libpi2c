3bf239566967ad1630c82732ac339f5a93e8a7e9
Paul Anderson <pha@umich.edu>
2021-01-03 16:52:21 -0500

    add mutex to all i2c bus operations

--- i2cbus.cpp
+++ i2cbus.cpp
@@ -16,6 +16,7 @@ bcm2835::bcm2835() {
 	bcm2835_i2c_begin();
 	bcm2835_i2c_set_baudrate(100000);
 }
+
 bcm2835::~bcm2835() {
 	bcm2835_i2c_end();
 }
@@ -59,6 +60,7 @@ void i2cBus::write(uint8_t targetAddress, const uint8_t ch) {
 }
 
 void i2cBus::write(uint8_t targetAddress, const uint8_t *buf, uint32_t len) {
+	const std::lock_guard<std::mutex> lock(mutex);
 	bcm2835_i2c_setSlaveAddress(targetAddress);
 	if(bcm2835_i2c_write(const_cast<char *>(reinterpret_cast<const char *>(buf)), len) != BCM2835_I2C_REASON_OK)
 		throw i2cException("bcm2835_i2c_write failed");
@@ -69,12 +71,14 @@ void i2cBus::read(uint8_t targetAddress, uint8_t *buf) {
 }
 
 void i2cBus::read(uint8_t targetAddress, uint8_t *buf, uint32_t len) {
+	const std::lock_guard<std::mutex> lock(mutex);
 	bcm2835_i2c_setSlaveAddress(targetAddress);
 	if(bcm2835_i2c_read(const_cast<char *>(reinterpret_cast<const char *>(buf)), len) != BCM2835_I2C_REASON_OK)
 		throw i2cException("bcm2835_i2c_write failed");
 }
 
 void i2cBus::readRegisterWithRestart(uint8_t targetAddress, uint8_t register_, uint8_t *buf, uint32_t len) {
+	const std::lock_guard<std::mutex> lock(mutex);
 	bcm2835_i2c_setSlaveAddress(targetAddress);
 	auto rc = bcm2835_i2c_read_register_rs(reinterpret_cast<char *>(&register_), reinterpret_cast<char *>(buf), len);
 	if(rc != BCM2835_I2C_REASON_OK)
